06bb4a475067ef04fd9f1182f863211056b41847
BCA202100923 <202100923@sigc.edu>
2024-03-09 04:10:42 -0800

    update

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="home-container">
       <h3>Welcome to React-App</h3>
-      <img className="home-img" src="" />
+      <img className="home-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6A6IGT5_CvLOEeH6q_pmBoRugM71P6-oCKlOm7rCgyA&s" />
     </div>
   )
 }
